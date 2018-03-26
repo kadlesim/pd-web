e9eb51532aa9dbba049da68be7903e1fa62021ba
kadlesim <kadlesim@fel.cvut.cz>
2018-03-26 17:03:02 +0200

    ?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {EventsService} from "./evets.service";
+import {Event} from "./event";
+import {log} from "util";
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'pd';
+
+  observableEvents: Observable<Event[]>
+  events: Event[];
+  errorMessage: String;
+  constructor(private eventService: EventsService) { }
+  ngOnInit(): void {
+    this.observableEvents = this.eventService.getEventsWithObservable();
+    this.observableEvents.subscribe(
+      events => this.events = events,
+      error =>  this.errorMessage = <any>error);
+    log(this.events[0].caption);
+    log('sem tu');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {EventsService} from "./evets.service";
+// import {}
 
 
 @NgModule({
@@ -10,9 +12,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    EventsService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
